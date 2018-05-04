fef4abaf1229e43136fb7e14d1023e128a0b74f3
Shivangi Garg <gargshivangi@gmail.com>
2018-05-04 17:02:58 +0530

    first commit

---
+++ MVCDemo_DisplayEmpDetails/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo_DisplayEmpDetails.Models;
+
+namespace MVCDemo_DisplayEmpDetails.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeID == id);
+            return View(employee);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create_Post()
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee emp = new Employee();
+            UpdateModel(emp);
+            employeeContext.Employees.Add(emp);
+            employeeContext.SaveChanges();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeID == id);
+            return View(employee);
+        }
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult Save()
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee emp = new Employee();
+            UpdateModel(emp);
+            employeeContext.Entry(emp).State = System.Data.Entity.EntityState.Modified;
+            employeeContext.SaveChanges();
+
+            return View(emp);
+        }
+
+        
+    }
+}
---
+++ MVCDemo_DisplayEmpDetails/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo_DisplayEmpDetails.Models
+{
+    [Table("tblEmployee")]
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+}
